ee2e668ab148a538ba5a365d0aab67cb85249b98
tkelly <tkelly@scottlogic.co.uk>
2014-10-22 17:15:27 +0100

    Introduce UserVoteController
    Fetches votes for a given user.

---
+++ VotingApplication/VotingApplication.Web.Api.Tests/Controllers/UserVoteControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FakeDbSet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+using VotingApplication.Web.Api.Controllers;
+
+namespace VotingApplication.Web.Api.Tests.Controllers
+{
+    [TestClass]
+    public class UserVoteControllerTests
+    {
+        private UserVoteController _controller;
+        private Vote _bobVote;
+        private Vote _joeVote;
+
+        [TestInitialize]
+        public void setup()
+        {
+            Option burgerOption = new Option { Id = 1, Name = "Burger King" };
+            User bobUser = new User { Id = 1, Name = "Bob" };
+            User joeUser = new User { Id = 2, Name = "Joe" };
+
+            InMemoryDbSet<User> dummyUsers = new InMemoryDbSet<User>(true);
+            InMemoryDbSet<Vote> dummyVotes = new InMemoryDbSet<Vote>(true);
+
+            _bobVote = new Vote() { Id = 1, Option = burgerOption, User = bobUser, UserId = 1 };
+            dummyUsers.Add(bobUser);
+            dummyVotes.Add(_bobVote);
+
+            _joeVote = new Vote() { Id = 2, Option = burgerOption, User = joeUser, UserId = 2 };
+            dummyUsers.Add(joeUser);
+            dummyVotes.Add(_joeVote);
+
+            var mockContextFactory = new Mock<IContextFactory>();
+            var mockContext = new Mock<IVotingContext>();
+            mockContextFactory.Setup(a => a.CreateContext()).Returns(mockContext.Object);
+            mockContext.Setup(a => a.Votes).Returns(dummyVotes);
+            mockContext.Setup(b => b.Users).Returns(dummyUsers);
+
+            _controller = new UserVoteController(mockContextFactory.Object);
+            _controller.Request = new HttpRequestMessage();
+            _controller.Configuration = new HttpConfiguration();
+        }
+
+        [TestMethod]
+        public void GetByUserIdReturnsVotes()
+        {
+            // Act
+            var response = _controller.Get(1);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetByUserIdReturnsVotesForThatUser()
+        {
+            // Act
+            var response = _controller.Get(1);
+            List<Vote> responseVotes = ((ObjectContent)response.Content).Value as List<Vote>;
+
+            // Assert
+            List<Vote> expectedVotes = new List<Vote>();
+            expectedVotes.Add(_bobVote);
+            CollectionAssert.AreEquivalent(expectedVotes, responseVotes);
+        }
+
+        [TestMethod]
+        public void GetByUserIdReturns404ForUnknownUser()
+        {
+            // Act
+            var response = _controller.Get(3);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetVoteForUserByVoteIdReturnsVote()
+        {
+            // Act
+            var response = _controller.Get(1, 1);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetVoteForUserByVoteIdReturnsVoteMatchingId()
+        {
+            // Act
+            var response = _controller.Get(2, 2);
+            Vote responseVote = ((ObjectContent)response.Content).Value as Vote;
+
+            // Assert
+            Assert.AreEqual(_joeVote, responseVote);
+        }
+
+        [TestMethod]
+        public void GetVoteForUserByVoteIdReturns404ForWrongUser()
+        {
+            // Act
+            var response = _controller.Get(1, 2);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ VotingApplication/VotingApplication.Web.Api/Controllers/UserVoteController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+
+namespace VotingApplication.Web.Api.Controllers
+{
+    public class UserVoteController : ApiController
+    {
+        private readonly IContextFactory _contextFactory;
+
+        public UserVoteController()
+        {
+            this._contextFactory = new ContextFactory();
+        }
+
+        public UserVoteController(IContextFactory contextFactory)
+        {
+            this._contextFactory = contextFactory;
+        }
+
+        #region GET
+
+        public HttpResponseMessage Get(long userId)
+        {
+            using (var context = _contextFactory.CreateContext())
+            {
+                var userExists = (context.Users.Where(u => u.Id == userId).Count() == 1);
+                if (userExists)
+                {
+                    return this.Request.CreateResponse(HttpStatusCode.OK, context.Votes.Where(v => v.UserId == userId).ToList<Vote>());
+                }
+                else
+                {
+                    return this.Request.CreateErrorResponse(HttpStatusCode.NotFound, string.Format("User {0} not found", userId));
+                }
+            }
+        }
+
+        public HttpResponseMessage Get(long userId, long voteId)
+        {
+            using (var context = _contextFactory.CreateContext())
+            {
+                var userExists = (context.Users.Where(u => u.Id == userId).Count() == 1);
+                if (userExists)
+                {
+                    var voteExists = context.Votes.Where(v => v.UserId == userId && v.Id == voteId).Count() == 1;
+                    if (voteExists)
+                    {
+                        return this.Request.CreateResponse(HttpStatusCode.OK, context.Votes.Where(v => v.UserId == userId && v.Id == voteId).FirstOrDefault());
+                    }
+                    else
+                    {
+                        return this.Request.CreateErrorResponse(HttpStatusCode.NotFound, string.Format("Vote {0} not found", voteId));
+                    }
+                    
+                }
+                else
+                {
+                    return this.Request.CreateErrorResponse(HttpStatusCode.NotFound, string.Format("User {0} not found", userId));
+                }
+            }
+        }
+
+        #endregion
+    }
+}
