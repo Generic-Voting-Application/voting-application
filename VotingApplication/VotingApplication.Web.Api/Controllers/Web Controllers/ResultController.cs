d7a439ca53c84f61b24ddd5e830da13b34982cac
tkelly <tkelly@scottlogic.co.uk>
2014-10-24 16:38:03 +0100

    Add results page
    Adds a summary of all the current votes, grouped by option.

---
+++ VotingApplication/VotingApplication.Web.Api/Controllers/Web Controllers/ResultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VotingApplication.Web.Api.Controllers
+{
+    public class ResultController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Voting Results";
+            ViewBag.Script = "/Scripts/VoteAppPages/Results.js";
+
+            return View();
+        }
+    }
+}
---
+++ VotingApplication/VotingApplication.Web.Api/Scripts/VoteAppPages/Results.js
@@ -0,0 +1,48 @@
+﻿function ResultViewModel() {
+    var self = this;
+
+    self.votes = ko.observableArray();
+
+    self.countVotes = function(voteArray)
+    {
+        var totalCounts = [];
+        voteArray.forEach(function (vote) {
+            var optionName = vote.Option.Name;
+            var voter = vote.User.Name;
+
+            // Find a vote with the same Option.Name, if it exists.
+            var existingOption = totalCounts.filter(function (vote) { return vote.Name == optionName; }).pop();
+
+            if (existingOption) {
+                existingOption.Count++;
+                existingOption.Voters.push(voter);
+            }
+            else {
+                totalCounts.push({
+                    Name: optionName,
+                    Count: 1,
+                    Voters: [voter]
+                });
+            }
+        });
+        return totalCounts;
+    }
+
+    $(document).ready(function () {
+        // Get all options
+        $.ajax({
+            type: 'GET',
+            url: "/api/vote",
+
+            success: function (data) {
+                var groupedVotes = self.countVotes(data);
+
+                groupedVotes.forEach(function (vote) {
+                    self.votes.push(vote);
+                });
+            }
+        });
+    });
+}
+
+ko.applyBindings(new ResultViewModel());
