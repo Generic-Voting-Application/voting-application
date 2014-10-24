2b33bb596c881afa8ceb1ef65f73c4763dbc13f9
tkelly <tkelly@scottlogic.co.uk>
2014-10-24 11:23:51 +0100

    Hook up front-end for voting
    Adds a login and voting buttons for the options.

--- VotingApplication/VotingApplication.Web.Api/Scripts/Home.js
+++ VotingApplication/VotingApplication.Web.Api/Scripts/Home.js
@@ -1,9 +1,29 @@
 ﻿function HomeViewModel() {
     var self = this;
+    var userId = 0;
 
     self.options = ko.observableArray();
 
+    // Submit a vote
+    self.doVote = function (data, event) {
+        $.ajax({
+            type: 'PUT',
+            url: '/api/user/'+userId+'/vote',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                OptionId: data.Id
+            }),
+
+            success: function (returnData) {
+                var currentRow = event.currentTarget.parentElement.parentElement;
+                $(currentRow).siblings().removeClass("success");
+                $(currentRow).addClass("success");
+            }
+        });
+    }
+
     $(document).ready(function () {
+        // Get all options
         $.ajax({
             type: 'GET',
             url: "/api/option",
@@ -13,6 +33,24 @@
                     self.options.push(option);
                 });
             }
+        });
+
+        // Do login
+        $("#Name-submit").click(function () {
+            $.ajax({
+                type: 'PUT',
+                url: '/api/user',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    Name: $("#Name").val()
+                }),
+
+                success: function (data) {
+                    userId = data;
+                    $("#login-box").hide();
+                    $("#vote-table").show();
+                }
+            });
         });
     });
 }
