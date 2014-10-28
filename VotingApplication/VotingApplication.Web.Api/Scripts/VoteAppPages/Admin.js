9cc310501074c5aecd768a920874a4962e30a64b
tkelly <tkelly@scottlogic.co.uk>
2014-10-28 10:42:37 +0000

    Hook it up
    Makes the button call "DELETE /api/vote" to remove all votes.
    The button changes text and becomes disabled on clicking, to give feedback
    that the process has worked.

--- VotingApplication/VotingApplication.Web.Api/Scripts/VoteAppPages/Admin.js
+++ VotingApplication/VotingApplication.Web.Api/Scripts/VoteAppPages/Admin.js
@@ -1,1 +1,21 @@
-﻿
+﻿function AdminViewModel() {
+    var self = this;
+    $(document).ready(function () {
+
+        self.resetVotes = function () {
+            $.ajax({
+                type: 'DELETE',
+                url: "/api/vote",
+
+                success: function() {
+                    $("#reset-votes").attr('disabled', 'disabled');
+                    $("#reset-votes").text("Votes were reset");
+                }
+            });
+        }
+
+        $("#reset-votes").click(self.resetVotes);
+    });
+}
+
+ko.applyBindings(new AdminViewModel());
