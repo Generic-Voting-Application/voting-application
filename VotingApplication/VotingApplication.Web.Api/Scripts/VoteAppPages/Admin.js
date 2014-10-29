63d85ec63d7c6ef4a7b8d609db957750be76e827
tkelly <tkelly@scottlogic.co.uk>
2014-10-29 11:46:12 +0000

    Hook up single-vote deletion
    Adds the option to delete a single vote to the admin panel, via a votes
    table.

--- VotingApplication/VotingApplication.Web.Api/Scripts/VoteAppPages/Admin.js
+++ VotingApplication/VotingApplication.Web.Api/Scripts/VoteAppPages/Admin.js
@@ -1,5 +1,8 @@
 ﻿function AdminViewModel() {
     var self = this;
+
+    self.votes = ko.observableArray();
+
     $(document).ready(function () {
 
         self.resetVotes = function () {
@@ -7,14 +10,40 @@
                 type: 'DELETE',
                 url: "/api/vote",
 
-                success: function() {
+                success: function () {
                     $("#reset-votes").attr('disabled', 'disabled');
                     $("#reset-votes").text("Votes were reset");
+                    self.populateVotes();
+                }
+            });
+        }
+
+        self.deleteVote = function (data, event) {
+            $.ajax({
+                type: 'DELETE',
+                url: '/api/vote?id=' + data.Id,
+                contentType: 'application/json',
+
+                success: function () {
+                    self.populateVotes();
+                }
+            });
+        }
+
+        self.populateVotes = function () {
+            $.ajax({
+                type: 'GET',
+                url: "/api/vote",
+
+                success: function (data) {
+                    //Replace contents of self.votes with 'data'
+                    self.votes(data);
                 }
             });
         }
 
         $("#reset-votes").click(self.resetVotes);
+        self.populateVotes();
     });
 }
 
