4ba0e4a835e9676f096977c9940837a64634069c
tkelly <tkelly@scottlogic.co.uk>
2014-10-31 14:00:41 +0000

    Introduce basic SessionController
    Implements an empty implementation of the SessionController, where all
    methods return MethodNotAllowed.

---
+++ VotingApplication/VotingApplication.Web.Api.Tests/Controllers/SessionControllerTests.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using FakeDbSet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+using VotingApplication.Web.Api.Controllers.API_Controllers;
+
+namespace VotingApplication.Web.Api.Tests.Controllers
+{
+    [TestClass]
+    public class SessionControllerTests
+    {
+        private SessionController _controller;
+        private Session _mainSession;
+        private Session _otherSession;
+        private InMemoryDbSet<Session> _dummySessions;
+
+        [TestInitialize]
+        public void setup()
+        {
+            _mainSession = new Session() { Id = 1 };
+            _otherSession = new Session() { Id = 2 };
+
+            _dummySessions = new InMemoryDbSet<Session>(true);
+            _dummySessions.Add(_mainSession);
+            _dummySessions.Add(_otherSession);
+
+            var mockContextFactory = new Mock<IContextFactory>();
+            var mockContext = new Mock<IVotingContext>();
+            mockContextFactory.Setup(a => a.CreateContext()).Returns(mockContext.Object);
+            mockContext.Setup(a => a.Sessions).Returns(_dummySessions);
+
+            _controller = new SessionController(mockContextFactory.Object);
+            _controller.Request = new HttpRequestMessage();
+            _controller.Configuration = new HttpConfiguration();
+        }
+
+        #region GET
+
+        [TestMethod]
+        public void GetIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Get();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Get(1);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+
+        #region PUT
+
+        [TestMethod]
+        public void PutIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Put(new Session());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void PutByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Put(1, new Session());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+
+        #region POST
+
+        [TestMethod]
+        public void PostIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Post(new Session());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void PostByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Post(1, new Session());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+
+        #region DELETE
+
+        [TestMethod]
+        public void DeleteIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Delete();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void DeleteByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Delete(1);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+
+    }
+}
---
+++ VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/SessionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+
+namespace VotingApplication.Web.Api.Controllers.API_Controllers
+{
+    public class SessionController : WebApiController
+    {
+        public SessionController() : base() { }
+        public SessionController(IContextFactory contextFactory) : base(contextFactory) { }
+
+        #region Put
+
+        public virtual HttpResponseMessage Put(object obj)
+        {
+            return this.Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "Cannot use PUT on this controller");
+        }
+
+        #endregion
+
+        #region Post
+
+        public virtual HttpResponseMessage Post(object obj)
+        {
+            return this.Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "Cannot use POST on this controller");
+        }
+
+        #endregion
+    }
+}
