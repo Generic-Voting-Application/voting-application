f1d342dc21eaf26e66c273ee3532e481202ae9ce
Steven Hillcox <shillcox@scottlogic.co.uk>
2014-11-04 12:17:58 +0000

    Add Vote page to front-end
    - Moves voting to separate page
    - UserId now stored in localstorage
    - UI made more generic ('Option' instead of 'Restaurant' etc)

---
+++ VotingApplication/VotingApplication.Web.Api/Controllers/Web Controllers/VoteController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace VotingApplication.Web.Api.Controllers.Web_Controllers
+{
+    public class VoteController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Voting";
+            ViewBag.Script = "/Scripts/VoteAppPages/Vote.js";
+
+            return View();
+        }
+    }
+}
