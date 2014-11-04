721ab83a0b6234cd324f8fd3344394ec06e591e7
tkelly <tkelly@scottlogic.co.uk>
2014-11-04 15:24:05 +0000

    Introduce empty OptionSetController
    Adds a controller which marks all requests as MethodNotAllowed

---
+++ VotingApplication/VotingApplication.Web.Api.Tests/Controllers/OptionSetControllerTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+using VotingApplication.Web.Api.Controllers.API_Controllers;
+
+namespace VotingApplication.Web.Api.Tests.Controllers
+{
+    [TestClass]
+    public class OptionSetControllerTests
+    {
+        private OptionSetController _controller;
+
+        [TestInitialize]
+        public void setup()
+        {
+            var mockContextFactory = new Mock<IContextFactory>();
+            var mockContext = new Mock<IVotingContext>();
+            mockContextFactory.Setup(a => a.CreateContext()).Returns(mockContext.Object);
+
+            _controller = new OptionSetController(mockContextFactory.Object);
+            _controller.Request = new HttpRequestMessage();
+            _controller.Configuration = new HttpConfiguration();
+        }
+
+        #region GET
+
+        [TestMethod]
+        public void GetIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Get();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+
+        [TestMethod]
+        public void GetByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Get(1);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+
+        #region POST
+
+        [TestMethod]
+        public void PostIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Post(new OptionSet());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void PostByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Post(1, new OptionSet());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+
+        #region PUT
+
+        [TestMethod]
+        public void PutIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Put(new OptionSet());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void PutByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Put(1, new OptionSet());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+
+        #region DELETE
+
+        [TestMethod]
+        public void DeleteIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Delete();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void DeleteByIdIsNotAllowed()
+        {
+            // Act
+            var response = _controller.Delete(1);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
+    }
+}
---
+++ VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/OptionSetController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+
+namespace VotingApplication.Web.Api.Controllers.API_Controllers
+{
+    public class OptionSetController : WebApiController
+    {
+        public OptionSetController() : base() { }
+        public OptionSetController(IContextFactory contextFactory) : base(contextFactory) { }
+
+        #region Get
+
+        public virtual HttpResponseMessage Get(long id)
+        {
+            return this.Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "Cannot use GET by Id on this controller");
+        }
+
+        #endregion
+
+        #region Put
+
+        public virtual HttpResponseMessage Put(OptionSet newOptionSet)
+        {
+            return this.Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "Cannot use PUT on this controller");
+        }
+
+        #endregion
+
+        #region Post
+
+        public virtual HttpResponseMessage Post(OptionSet newOptionSet)
+        {
+            return this.Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "Cannot use POST on this controller");
+        }
+
+        #endregion
+    }
+}
