d39e3400f13df2c10ad71845440d7ba0e46af176
tkelly <tkelly@scottlogic.co.uk>
2014-11-05 11:32:05 +0000

    Enable automatic database migrations
    Allows the database to automatically update when the data models change,
    without us having to drop the database.
    
    See http://geekswithblogs.net/michelotti/archive/2012/11/13/enable-automatic-code-first-migrations-on-sql-database-in-azure.aspx

--- VotingApplication/VotingApplication.Web.Api/Global.asax.cs
+++ VotingApplication/VotingApplication.Web.Api/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -7,6 +8,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Newtonsoft.Json;
+using VotingApplication.Data.Context;
+using VotingApplication.Web.Api.Migrations;
 
 namespace VotingApplication.Web.Api
 {
@@ -23,6 +26,10 @@ namespace VotingApplication.Web.Api
             // Fix the infinite recursion of Session.OptionSet.Options[0].OptionSets[0].Options[0].[...]
             // by not populating the Option.OptionSets after already encountering Session.OptionSet
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            //Enable automatic migrations
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<VotingContext, Configuration>());
+            new VotingContext().Database.Initialize(false);
         }
     }
 }
