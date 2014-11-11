688ec4c53e68e6e48d974639e2b52f99c8b87b6e
Steven Hillcox <shillcox@scottlogic.co.uk>
2014-11-11 13:50:58 +0000

    Another small fix

--- VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/SessionVoteController.cs
+++ VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/SessionVoteController.cs
@@ -57,12 +57,12 @@ namespace VotingApplication.Web.Api.Controllers.API_Controllers
 
         #region PUT
 
-        public virtual HttpResponseMessage Put(long sessionId, Vote vote)
+        public virtual HttpResponseMessage Put(Guid sessionId, Vote vote)
         {
             return this.Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "Cannot use PUT on this controller");
         }
 
-        public virtual HttpResponseMessage Put(long sessionId, long voteId, Vote vote)
+        public virtual HttpResponseMessage Put(Guid sessionId, long voteId, Vote vote)
         {
             return this.Request.CreateErrorResponse(HttpStatusCode.MethodNotAllowed, "Cannot use PUT by id on this controller");
         }
