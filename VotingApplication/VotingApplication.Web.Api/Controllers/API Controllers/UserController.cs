4d73e130ecfb54b18e30a7a79fd197d138c7a331
tkelly <tkelly@scottlogic.co.uk>
2014-12-17 12:11:53 +0000

    Generate tokens for all users logging in
    For closed polls, the users should have a token already.
    For open polls, it will check if they already have a token, and generate
    one if not.

--- VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/UserController.cs
+++ VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/UserController.cs
@@ -58,15 +58,39 @@ namespace VotingApplication.Web.Api.Controllers
                     return this.Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Unacceptable Username");
                 }
 
-                List<User> existingUsers = context.Users.Where(u => u.Name == newUser.Name).ToList<User>();
-                if (existingUsers.Count() != 0)
+                Poll matchingPoll = context.Polls.Where(p => p.UUID == newUser.PollId).FirstOrDefault();
+                if (matchingPoll == null)
                 {
-                    return this.Request.CreateResponse(HttpStatusCode.OK, existingUsers.FirstOrDefault().Id);
+                    return this.Request.CreateErrorResponse(HttpStatusCode.BadRequest, "User missing a poll");
                 }
 
-                context.Users.Add(newUser);
-                context.SaveChanges();
-                return this.Request.CreateResponse(HttpStatusCode.OK, newUser.Id);
+                Token matchingToken = context.Tokens.Where(t => t.TokenGuid == newUser.TokenId && t.PollId == matchingPoll.UUID).FirstOrDefault();
+                if (matchingToken == null || newUser.TokenId == Guid.Empty)
+                {
+                    if (matchingPoll.InviteOnly || newUser.TokenId != Guid.Empty)
+                    {
+                        return this.Request.CreateErrorResponse(HttpStatusCode.BadRequest, "User missing a valid token for this poll");
+                    }
+                    else
+                    {
+                        matchingToken = new Token() { TokenGuid = Guid.NewGuid(), PollId = matchingPoll.UUID };
+                        context.Tokens.Add(matchingToken);
+                    }
+                }
+
+                List<User> existingUsers = context.Users.Where(u => u.TokenId == matchingToken.TokenGuid).ToList<User>();
+                if (existingUsers.Count() == 0)
+                {
+                    //Save once to generate user ID
+                    context.Users.Add(newUser);
+                    context.SaveChanges();
+
+                    //Save again to store Id
+                    matchingToken.UserId = newUser.Id;
+                    context.SaveChanges();
+                }
+
+                return this.Request.CreateResponse(HttpStatusCode.OK, matchingToken);
             }
         }
 
