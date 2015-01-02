f8544f43f32fa53a02aa29059d9889b74df634d2
tkelly <tkelly@scottlogic.co.uk>
2015-01-02 12:02:31 +0000

    Update timestamp on voting
    When a vote is cast in a poll, that poll is marked as updated. This ties
    in to the requesting of results for polls, where we check whether or not
    the data is new.

--- VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/UserVoteController.cs
+++ VotingApplication/VotingApplication.Web.Api/Controllers/API Controllers/UserVoteController.cs
@@ -137,6 +137,8 @@ namespace VotingApplication.Web.Api.Controllers
                     }
 
                     vote.UserId = userId;
+
+                    poll.LastUpdated = DateTime.UtcNow;
                 }
 
                 foreach (Vote vote in votes)
