dcb280a2300d546caf082af24cb5dab1e6c1c2dc
tkelly <tkelly@scottlogic.co.uk>
2015-01-09 10:59:23 +0000

    Fix for users adding options
    Common.getPollId no longer exists, so the users were not able to add
    options properly.

--- VotingApplication/VotingApplication.Web.Api/Scripts/VotingStrategies/PointsVote.js
+++ VotingApplication/VotingApplication.Web.Api/Scripts/VotingStrategies/PointsVote.js
@@ -115,7 +115,7 @@
         var refreshOptions = function () {
             $.ajax({
                 type: 'GET',
-                url: "/api/poll/" + pollData.UUID + "/option",
+                url: "/api/poll/" + pollId + "/option",
 
                 success: function (data) {
                     var newOptionCount = data.length - self.options().length;
@@ -268,7 +268,7 @@
 
             $.ajax({
                 type: 'POST',
-                url: '/api/poll/' + Common.getPollId() + '/option',
+                url: '/api/poll/' + pollId + '/option',
                 contentType: 'application/json',
 
                 data: JSON.stringify({
