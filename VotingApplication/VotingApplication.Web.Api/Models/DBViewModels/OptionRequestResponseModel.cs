24e9c04b33837f3a5de0747eb868a10741da0a15
Andrew Lee <alee@scottlogic.co.uk>
2015-01-20 09:45:48 +0000

    Fixed issue with poll options not having Ids for the client to vote with

--- VotingApplication/VotingApplication.Web.Api/Models/DBViewModels/OptionRequestResponseModel.cs
+++ VotingApplication/VotingApplication.Web.Api/Models/DBViewModels/OptionRequestResponseModel.cs
@@ -4,6 +4,7 @@ namespace VotingApplication.Web.Api.Models.DBViewModels
 {
     public class OptionRequestResponseModel
     {
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Info { get; set; }
