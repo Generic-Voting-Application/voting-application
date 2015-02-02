b0abcda2dec53be2e5b1d2f095c07817f9e5b7d4
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-02-02 11:05:52 +0000

    Typo

--- VotingApplication/VotingApplication.Web.Api/Validators/PointsVoteValidator.cs
+++ VotingApplication/VotingApplication.Web.Api/Validators/PointsVoteValidator.cs
@@ -18,7 +18,7 @@ namespace VotingApplication.Web.Api.Validators
 
             if (voteRequests.Any(v => v.VoteValue > poll.MaxPerVote || v.VoteValue <= 0) || voteRequests.Sum(v => v.VoteValue) > poll.MaxPoints)
             {
-                modelState.AddModelError("VoteValue", String.Format("Invalid vote value, vote values must be between 1 and {0} with thier total not exceeding {1}", 
+                modelState.AddModelError("VoteValue", String.Format("Invalid vote value, vote values must be between 1 and {0} with their total not exceeding {1}", 
                                                                     poll.MaxPerVote, poll.MaxPoints));
             }
         }
