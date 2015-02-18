44c5db1289f6478414c16223cddb1c44ac550688
tkelly <tkelly@scottlogic.co.uk>
2015-02-18 11:33:06 +0000

    Display the correct poll name
    Pulls the name of the poll from the /poll/{pollId} ajax call, and
    displays it.

--- VotingApplication/VotingApplication.Web/Scripts/Controllers/HomePageController.js
+++ VotingApplication/VotingApplication.Web/Scripts/Controllers/HomePageController.js
@@ -1,10 +1,14 @@
 ﻿(function () {
     var VotingApp = angular.module('VotingApp');
 
-    VotingApp.controller('HomePageController', function ($scope) {
+    VotingApp.controller('HomePageController', ['$scope', 'PollAction', function ($scope, PollAction) {
         $scope.models = {
             pageTitle: 'Pollster'
         };
-    });
+
+        PollAction.getPoll(PollAction.currentPollId(), function (data) {
+            $scope.pollName = data.Name
+        })
+    }]);
 
 })();
