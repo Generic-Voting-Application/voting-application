193ef20fc8d8595f2668b31542686617c3c6737d
tkelly <tkelly@scottlogic.co.uk>
2015-02-19 10:59:31 +0000

    Pull out expiry string to service
    Creates a service to generate our time expiry countdown.

--- VotingApplication/VotingApplication.Web/Scripts/Controllers/HomePageController.js
+++ VotingApplication/VotingApplication.Web/Scripts/Controllers/HomePageController.js
@@ -1,57 +1,16 @@
 ﻿(function () {
     var VotingApp = angular.module('VotingApp');
 
-    VotingApp.controller('HomePageController', ['$scope', 'PollService', function ($scope, PollService) {
+    VotingApp.controller('HomePageController', ['$scope', 'PollService', 'ExpiryStringService', function ($scope, PollService, ExpiryStringService) {
         $scope.models = {
             pageTitle: 'Pollster'
         };
 
         $scope.pollExpiry = undefined;
 
-        var timeStringForTimeout = function (expiryDate, timeUnit, timeUnitName) {
-            var secondsToExpiry = Math.floor((expiryDate - Date.now()) / 1000);
-
-            var timeValue = Math.floor(secondsToExpiry / timeUnit);
-            timeUnitName = (timeValue == 1) ? timeUnitName : timeUnitName + 's';
-
-            var timeString = timeValue + ' ' + timeUnitName;
-
-            //Update at the next point we expect a change
-            var timeoutTime = 1000 * (secondsToExpiry - 1 - timeValue * timeUnit);
-            setTimeout(calculateExpiry, timeoutTime, expiryDate);
-
-            return timeString;
-        }
-
         var calculateExpiry = function (expiryDate) {
-            if (expiryDate < Date.now()) {
-                $scope.pollExpiry = 'Poll closed';
-                $scope.$apply();
-            } else {
-                var timeString;
-
-                var secondsToExpiry = (expiryDate - Date.now()) / 1000;
-
-                //NB: Months is an approximation, we don't need to be absolutely exact here.
-                if (secondsToExpiry > 60 * 60 * 24 * 28) {
-                    timeString = timeStringForTimeout(expiryDate, 60 * 60 * 24 * 28, 'month');
-                }
-                else if (secondsToExpiry > 60 * 60 * 24) {
-                    timeString = timeStringForTimeout(expiryDate, 60 * 60 * 24, 'day');
-                }
-                else if (secondsToExpiry > 60 * 60) {
-                    timeString = timeStringForTimeout(expiryDate, 60 * 60, 'hour');
-                }
-                else if (secondsToExpiry > 60) {
-                    timeString = timeStringForTimeout(expiryDate, 60, 'minute');
-                }
-                else {
-                    timeString = timeStringForTimeout(expiryDate, 1, 'second');
-                }
-
-                $scope.pollExpiry = 'Poll closes in: ' + timeString;
-                $scope.$apply();
-            }
+            $scope.pollExpiry = ExpiryStringService.timeStringForExpiry(expiryDate, calculateExpiry);
+            $scope.$apply();
         }
 
         PollService.getPoll(PollService.currentPollId(), function (data) {
