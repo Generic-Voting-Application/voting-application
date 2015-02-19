97af6eb0bf44da48bd57991b802d598cb17efd65
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-02-19 10:22:45 +0000

    - Adds voting for basic vote - Adds ninject to project

--- VotingApplication/VotingApplication.Web/Scripts/Controllers/BasicVoteController.js
+++ VotingApplication/VotingApplication.Web/Scripts/Controllers/BasicVoteController.js
@@ -1,21 +1,39 @@
 ﻿(function () {
     var VotingApp = angular.module('VotingApp');
 
-    VotingApp.controller('BasicVoteController', ['$scope', 'IdentityService', 'PollService', function ($scope, IdentityService, PollService) {
+    VotingApp.controller('BasicVoteController', ['$scope', 'IdentityService', 'PollService', 'TokenService', function ($scope, IdentityService, PollService, TokenService) {
 
         var pollId = PollService.currentPollId();
         
-        $scope.vote = function (options) {
-            if (IdentityService.identityName) {
-                // Do the voting stuff here
-                console.log(options);
-            } else {
+        $scope.vote = function (option) {
+            if (!option) {
+                return null;
+            }
+
+            if (!IdentityService.identityName) {
                 IdentityService.openLoginDialog($scope);
+            } else if(!token) {
+                // Probably invite only, tell the user
+            } else {
+
+                votes = [{
+                    OptionId: option.Id,
+                    VoteValue: 1,
+                    VoterName: IdentityService.identityName
+                }];
+
+                PollService.submitVote(pollId, votes, token, function () {
+                    console.log("voted!");
+                });
             }
         }
 
         PollService.getPoll(pollId, function (data) {
             $scope.options = data.Options;
+        });
+
+        TokenService.getToken(pollId, function (data) {
+            token = data;
         });
 
     }]);
