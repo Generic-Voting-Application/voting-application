0dd2f0baac2a1134cfa52508e15eafc624b83456
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-03-02 11:12:49 +0000

    Backend support for poll updates via manage controller

--- VotingApplication/VotingApplication.Web/Api/Models/DBViewModels/ManagePollUpdateRequest.cs
+++ VotingApplication/VotingApplication.Web/Api/Models/DBViewModels/ManagePollUpdateRequest.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using VotingApplication.Data.Model;
 
 namespace VotingApplication.Web.Api.Models.DBViewModels
 {
-    public class ManagePollRequestResponseModel
+    public class ManagePollUpdateRequest
     {
-        public Guid UUID { get; set; }
-        public List<Option> Options { get; set; }
-        public string VotingStrategy { get; set; }
-        public int MaxPoints { get; set; }
-        public int MaxPerVote { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        public string VotingStrategy { get; set; }
+        [Range(1, int.MaxValue)]
+        public int MaxPoints { get; set; }
+        [Range(1, int.MaxValue)]
+        public int MaxPerVote { get; set; }
+        public List<Option> Options { get; set; }
         public bool InviteOnly { get; set; }
         public bool AnonymousVoting { get; set; }
         public bool RequireAuth { get; set; }
