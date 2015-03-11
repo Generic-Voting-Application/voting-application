545e4c3da2c2d9c504350c1c1aba7f410c106e4f
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-03-11 13:51:17 +0000

    Basic poll type change page created. Few fixes to basic vote.

--- VotingApplication/VotingApplication.Web/Api/Controllers/ManageController.cs
+++ VotingApplication/VotingApplication.Web/Api/Controllers/ManageController.cs
@@ -71,11 +71,11 @@ namespace VotingApplication.Web.Api.Controllers.API_Controllers
                 ModelState.AddModelError("ExpiryDate", "Invalid or unspecified ExpiryDate");
             }
 
-            if(updateRequest.Options != null)
+            if (updateRequest.Options != null)
             {
-                foreach(Option option in updateRequest.Options)
+                foreach (Option option in updateRequest.Options)
                 {
-                    if(option.Name == null || option.Name == String.Empty)
+                    if (option.Name == null || option.Name == String.Empty)
                     {
                         ModelState.AddModelError("Option.Name", "Invalid or unspecified Option Name");
                     }
@@ -91,34 +91,34 @@ namespace VotingApplication.Web.Api.Controllers.API_Controllers
 
             using (var context = _contextFactory.CreateContext())
             {
-                Poll existingPoll = context.Polls
+                Poll poll = context.Polls
                                            .Where(p => p.ManageId == manageId)
                                            .Include(p => p.Options)
                                            .SingleOrDefault();
 
-                if (existingPoll == null)
+                if (poll == null)
                 {
                     this.ThrowError(HttpStatusCode.NotFound, string.Format("Poll for manage id {0} not found", manageId));
                 }
 
-                existingPoll.NamedVoting = updateRequest.NamedVoting;
-                existingPoll.Expires = updateRequest.Expires;
-                existingPoll.ExpiryDate = updateRequest.ExpiryDate;
-                existingPoll.InviteOnly = updateRequest.InviteOnly;
-                existingPoll.MaxPerVote = updateRequest.MaxPerVote;
-                existingPoll.MaxPoints = updateRequest.MaxPoints;
-                existingPoll.Name = updateRequest.Name;
-                existingPoll.OptionAdding = updateRequest.OptionAdding;
-                existingPoll.RequireAuth = updateRequest.RequireAuth;
+                poll.NamedVoting = updateRequest.NamedVoting;
+                poll.Expires = updateRequest.Expires;
+                poll.ExpiryDate = updateRequest.ExpiryDate;
+                poll.InviteOnly = updateRequest.InviteOnly;
+                poll.MaxPerVote = updateRequest.MaxPerVote;
+                poll.MaxPoints = updateRequest.MaxPoints;
+                poll.Name = updateRequest.Name;
+                poll.OptionAdding = updateRequest.OptionAdding;
+                poll.RequireAuth = updateRequest.RequireAuth;
 
                 List<Option> newOptions = new List<Option>();
                 List<Option> oldOptions = new List<Option>();
-                List<Vote> oldVotes = new List<Vote>();
+                List<Vote> removedVotes = new List<Vote>();
 
                 if (updateRequest.Options != null && updateRequest.Options.Count > 0)
                 {
                     // Match up duplicates and clear out votes of options that are deleted
-                    foreach (Option option in existingPoll.Options)
+                    foreach (Option option in poll.Options)
                     {
                         Option duplicateRequestOption = updateRequest.Options.Find(o => o.Id == option.Id);
 
@@ -133,29 +133,31 @@ namespace VotingApplication.Web.Api.Controllers.API_Controllers
                         else
                         {
                             oldOptions.Add(option);
-                            oldVotes.AddRange(context.Votes.Where(v => v.OptionId == option.Id).ToList());
+                            removedVotes.AddRange(context.Votes.Where(v => v.OptionId == option.Id).ToList());
                         }
                     }
 
                     newOptions.AddRange(updateRequest.Options);
                 }
 
+                poll.Options = newOptions;
+                poll.LastUpdated = DateTime.Now;
 
-
-                existingPoll.Options = newOptions;
-                existingPoll.LastUpdated = DateTime.Now;
+                if (updateRequest.VotingStrategy.ToLower() != poll.PollType.ToString().ToLower())
+                {
+                    removedVotes.AddRange(context.Votes.Where(v => v.PollId == poll.UUID).ToList());
+                    poll.PollType = (PollType)Enum.Parse(typeof(PollType), updateRequest.VotingStrategy, true);
+                }
 
                 foreach (Option oldOption in oldOptions)
                 {
                     context.Options.Remove(oldOption);
                 }
 
-                foreach (Vote oldVote in oldVotes)
+                foreach (Vote oldVote in removedVotes)
                 {
                     context.Votes.Remove(oldVote);
                 }
-
-                // Need code to handle poll type changed when enabaled.
 
                 context.SaveChanges();
             }
---
+++ VotingApplication/VotingApplication.Web/Scripts/Controllers/ManagePollTypeController.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    angular
+        .module('GVA.Creation')
+        .controller('ManagePollTypeController', ManageVotersController);
+
+    ManageVotersController.$inject = ['$scope', '$routeParams', '$location', 'ManageService'];
+
+    function ManageVotersController($scope, $routeParams, $location, ManageService) {
+
+        $scope.poll = ManageService.poll;
+        $scope.manageId = $routeParams.manageId;
+
+        $scope.updatePoll = updatePoll;
+        $scope.return = returnToManage;
+
+        activate();
+
+        function updatePoll() {
+            ManageService.updatePoll($routeParams.manageId, $scope.poll, function () {
+                ManageService.getPoll($scope.manageId);
+            });
+        }
+
+        function returnToManage() {
+            $location.path('Manage/' + $scope.manageId);
+        }
+
+        function activate() {
+            ManageService.registerPollObserver(function () {
+                $scope.poll = ManageService.poll;
+            })
+        }
+    };
+})();
