1dbcd95e25c6920fc7d8db7f4b06d21addb97ef1
tkelly <tkelly@scottlogic.co.uk>
2015-03-19 14:16:03 +0000

    Use routeParams for vote/result links
    Makes things a bit clearer what the values from the URL are supposed to
    represent.

--- VotingApplication/VotingApplication.Web/Scripts/Controllers/VotingPageController.js
+++ VotingApplication/VotingApplication.Web/Scripts/Controllers/VotingPageController.js
@@ -5,15 +5,15 @@
         .controller('VotingPageController', VotingPageController);
 
 
-    VotingPageController.$inject = ['$scope', '$location', 'IdentityService'];
+    VotingPageController.$inject = ['$scope', '$routeParams', 'IdentityService'];
 
-    function VotingPageController($scope, $location, IdentityService) {
+    function VotingPageController($scope, $routeParams, IdentityService) {
 
         // Turn "/#/voting/abc/123" into "/#/results/abc/123"
-        var locationTokens = $location.url().split("/");
-        locationTokens.splice(0, 2);
+        var pollId = $routeParams['pollId'];
+        var tokenId = $routeParams['tokenId'] || '';
 
-        $scope.resultsLink = '#/Results/' + locationTokens.join("/");
+        $scope.resultsLink = '#/Results/' + pollId + "/" + tokenId;
         $scope.identityName = IdentityService.identity ? IdentityService.identity.name : null;
         $scope.logoutIdentity = IdentityService.clearIdentityName;
 
@@ -23,7 +23,7 @@
         function activate() {
             // Angular won't auto update this so we need to use the observer pattern
             IdentityService.registerIdentityObserver(function () {
-                $scope.identityName = IdentityService.identity ? IdentityService.identity.name : null;;
+                $scope.identityName = IdentityService.identity ? IdentityService.identity.name : null;
             });
         }
 
