c3592789e49c73d89796edd60c3d480ad6caec53
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-03-23 12:15:51 +0000

    Adds routing service

---
+++ VotingApplication/VotingApplication.Web/Scripts/Services/RoutingService.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    angular
+        .module('GVA.Common')
+        .factory('RoutingService', RoutingService);
+
+    function RoutingService() {
+
+        var service = {
+            navigateToVotePage: navigateToVotePage,
+            navigateToResultsPage: navigateToResultsPage,
+            navigateToManagePage: navigateToManagePage,
+            navigateToHomePage: navigateToHomePage
+        };
+
+        return service;
+
+        function navigateToVotePage(pollId) {
+            $window.location.href = 'Poll/#/Vote' + pollId;
+        }
+
+        function navigateToResultsPage(pollId) {
+            $window.location.href = 'Poll/#/Results' + pollId;
+        }
+
+        function navigateToManagePage(manageId, subPage) {
+            $window.location.href = 'Manage/#/' + manageId + (subPage ? '/' + subPage : '');
+        }
+
+        function navigateToHomePage() {
+            $window.location.href = '';
+        }
+    }
+})();
