14d726990072a7b9039f4b10b98bcf409f2891d6
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-03-24 15:04:51 +0000

    Adds directive for truncating text

---
+++ VotingApplication/VotingApplication.Web/Scripts/Directives/TruncatedText.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    angular
+        .module('GVA.Common')
+        .directive('truncatedText', truncatedText);
+
+    truncatedText.$inject = ['$parse'];
+
+    function truncatedText($parse) {
+
+        function truncateText(text, limit) {
+
+            var words = text.split(' ');
+
+            var truncatedtext = words.reduce(function (prev, curr) {
+                if (curr.length + prev.length >= limit) {
+                    return prev;
+                } else {
+                    return prev + ' ' + curr;
+                }
+            });
+
+            return truncatedtext;
+        }
+
+        function link(scope, element, attrs) {
+
+            activate();
+
+            function activate() {
+
+                scope.maxChars = parseInt(scope.maxChars) || 160;
+                scope.fullText = '';
+                scope.truncatedText = '';
+
+                scope.$watch('ngModel', function () {
+                    scope.fullText = scope.ngModel;
+                    scope.truncatedText = truncateText(scope.fullText, scope.maxChars);
+                    scope.truncated = scope.fullText != scope.truncatedText;
+                });
+            }
+        }
+
+        return {
+            restrict: 'A',
+            scope: {
+                ngModel: '=',
+                maxChars: '@',
+            },
+            link: link,
+            template: '<span ng-if="!truncated">{{fullText}}</span>' +
+                      '<span ng-if="truncated">{{truncatedText}}</span>' +
+                      '<span ng-show="truncated">... <a style="cursor:pointer" ng-click="truncated=false">Show More</a></span>'
+        };
+    }
+})();
