c5e03e15419ac30d91627903ea869eafcc170bb2
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-04-10 09:54:31 +0100

    Adds test for misc manage controller

---
+++ VotingApplication/VotingApplication.Web.Api.Tests/Controllers/ManageMiscControllerTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.Entity;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+using VotingApplication.Web.Api.Controllers;
+using VotingApplication.Web.Api.Models.DBViewModels;
+using VotingApplication.Web.Api.Tests.TestHelpers;
+
+namespace VotingApplication.Web.Api.Tests.Controllers
+{
+    [TestClass]
+    public class ManageMiscControllerTests
+    {
+        public readonly Guid PollManageGuid = new Guid("961efb70-6767-4658-a95d-fea312c802ec");
+
+        [TestClass]
+        public class PutTests : ManageMiscControllerTests
+        {
+            [TestMethod]
+            [ExpectedHttpResponseException(HttpStatusCode.NotFound)]
+            public void UnknownManageId_ReturnsNotFound()
+            {
+                IDbSet<Poll> existingPolls = DbSetTestHelper.CreateMockDbSet<Poll>();
+                IContextFactory contextFactory = ContextFactoryTestHelper.CreateContextFactory(existingPolls);
+                ManagePollMiscRequest request = new ManagePollMiscRequest { };
+
+                ManageMiscController controller = CreateManageExpiryController(contextFactory);
+
+                controller.Put(Guid.NewGuid(), request);
+            }
+
+            [TestMethod]
+            public void SetMiscConfig_SetsMiscSettings()
+            {
+                IDbSet<Poll> existingPolls = DbSetTestHelper.CreateMockDbSet<Poll>();
+                Poll existingPoll = CreatePoll();
+                existingPolls.Add(existingPoll);
+
+                existingPoll.NamedVoting = false;
+                existingPoll.InviteOnly = false;
+
+                IContextFactory contextFactory = ContextFactoryTestHelper.CreateContextFactory(existingPolls);
+                ManagePollMiscRequest request = new ManagePollMiscRequest { InviteOnly = true, NamedVoting = true };
+
+                ManageMiscController controller = CreateManageExpiryController(contextFactory);
+
+                controller.Put(PollManageGuid, request);
+
+                Assert.IsTrue(existingPoll.InviteOnly);
+                Assert.IsTrue(existingPoll.NamedVoting);
+            }
+        }
+
+        public static ManageMiscController CreateManageExpiryController(IContextFactory contextFactory)
+        {
+            return new ManageMiscController(contextFactory)
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+        }
+
+        private Poll CreatePoll()
+        {
+            return new Poll()
+            {
+                ManageId = PollManageGuid
+            };
+        }
+    }
+}
