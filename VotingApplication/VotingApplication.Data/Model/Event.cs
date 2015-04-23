862ff93b951c9c1a1967c77905d9c0863cb09e80
tkelly <tkelly@scottlogic.co.uk>
2015-04-23 11:06:12 +0100

    Move EventType enum into its own file

--- VotingApplication/VotingApplication.Data/Model/Event.cs
+++ VotingApplication/VotingApplication.Data/Model/Event.cs
@@ -3,31 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace VotingApplication.Data.Model
 {
-    public enum EventType
-    {
-        Error,
-        GoToPage,
-        CreatePoll,
-        UpdateResults,
-        SetExpiry,
-        SetPollType,
-        SetQuestion,
-        SetInviteOnly,
-        SetNamedVoting,
-        SetOptionAdding,
-        SetHiddenResults,
-        AddOption,
-        UpdateOption,
-        DeleteOption,
-        AddVote,
-        DeleteVote,
-        AddBallot,
-        DeleteBallot,
-        Login,
-        Logout,
-        Register
-    }
-
     public class Event
     {
         public Event(EventType eventType, Guid pollId)
---
+++ VotingApplication/VotingApplication.Data/Model/EventType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotingApplication.Data.Model
+{
+    public enum EventType
+    {
+        Error,
+        GoToPage,
+        CreatePoll,
+        UpdateResults,
+        SetExpiry,
+        SetPollType,
+        SetQuestion,
+        SetInviteOnly,
+        SetNamedVoting,
+        SetOptionAdding,
+        SetHiddenResults,
+        AddOption,
+        UpdateOption,
+        DeleteOption,
+        AddVote,
+        DeleteVote,
+        AddBallot,
+        DeleteBallot,
+        Login,
+        Logout,
+        Register
+    }
+}
