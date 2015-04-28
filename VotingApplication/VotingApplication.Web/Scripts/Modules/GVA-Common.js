bcb65984cb866d5bc8746a483e4fe63c795a93f6
John Dawes <jdawes@scottlogic.co.uk>
2015-04-28 10:30:03 +0100

    Revert "Token Service uses promises"

--- VotingApplication/VotingApplication.Web/Scripts/Modules/GVA-Common.js
+++ VotingApplication/VotingApplication.Web/Scripts/Modules/GVA-Common.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('GVA.Common', ['ngDialog', 'ngStorage', 'ngRoute'])
+        .module('GVA.Common', ['ngDialog', 'ngStorage'])
         .config(['$httpProvider', function ($httpProvider) {
             $httpProvider.interceptors.push('ErrorInterceptor');
         }]);
