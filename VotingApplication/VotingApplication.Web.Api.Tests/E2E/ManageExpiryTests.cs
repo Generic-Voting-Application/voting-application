5d2cea55583dfdbbc82841b59566084ef7c7e84c
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-05-08 14:14:15 +0100

    Adds basic tests for expiry page

---
+++ VotingApplication/VotingApplication.Web.Api.Tests/E2E/ManageExpiryTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Protractor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VotingApplication.Data.Context;
+using VotingApplication.Data.Model;
+using VotingApplication.Web.Api.Tests.E2E.Helpers.Clearers;
+
+namespace VotingApplication.Web.Tests.E2E
+{
+    [TestClass]
+    public class ManageExpiryTests
+    {
+        private static readonly string ChromeDriverDir = @"..\..\";
+        private static readonly string SiteBaseUri = @"http://localhost:64205/";
+        private static readonly int WaitTime = 500;
+        private static readonly Guid PollGuid = Guid.NewGuid();
+        private static readonly Guid PollManageGuid = Guid.NewGuid();
+        private static readonly string PollUrl = SiteBaseUri + "Dashboard/#/Manage/" + PollManageGuid + "/Expiry";
+        private static readonly DateTimeOffset DefaultTime = DateTime.Now;
+
+        private ITestVotingContext _context;
+        private Poll _defaultPoll;
+        private IWebDriver _driver;
+
+
+        [TestInitialize]
+        public virtual void TestInitialise()
+        {
+            _context = new TestVotingContext();
+
+            // Open, Anonymous, No Option Adding, Shown Results
+            _defaultPoll = new Poll()
+            {
+                UUID = PollGuid,
+                ManageId = PollManageGuid,
+                PollType = PollType.Basic,
+                Name = "Test Poll",
+                LastUpdated = DateTime.Now,
+                CreatedDate = DateTime.Now,
+                Options = new List<Option>(),
+                InviteOnly = false,
+                NamedVoting = false,
+                OptionAdding = false,
+                HiddenResults = false,
+                ExpiryDate = DefaultTime
+            };
+
+            _context.Polls.Add(_defaultPoll);
+            _context.SaveChanges();
+
+            _driver = new NgWebDriver(new ChromeDriver(ChromeDriverDir));
+            _driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(10));
+            _driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(10));
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            _driver.Dispose();
+
+            PollClearer pollTearDown = new PollClearer(_context);
+            pollTearDown.ClearPoll(_defaultPoll);
+            _context.Dispose();
+        }
+
+        [TestMethod, TestCategory("E2E")]
+        public void ManagePollType_CancelButton_NavigatesToManagement()
+        {
+            _driver.Navigate().GoToUrl(PollUrl);
+
+            IWebElement cancelButton = _driver.FindElement(By.Id("cancel-button"));
+
+            cancelButton.Click();
+
+            Assert.AreEqual(SiteBaseUri + "Dashboard/#/Manage/" + _defaultPoll.ManageId, _driver.Url);
+        }
+
+        [TestMethod, TestCategory("E2E")]
+        public void ManagePollType_CancelButton_DoesNotSaveChanges()
+        {
+            _driver.Navigate().GoToUrl(PollUrl);
+
+            IWebElement hourAdjustButton = _driver.FindElement(By.Id("hour-adjust-button"));
+            hourAdjustButton.Click();
+
+            IWebElement cancelButton = _driver.FindElement(By.Id("cancel-button"));
+
+            cancelButton.Click();
+
+            Poll dbPoll = _context.Polls.Where(p => p.UUID == _defaultPoll.UUID).Single();
+
+            Assert.AreEqual(DefaultTime, dbPoll.ExpiryDate);
+        }
+    }
+}
