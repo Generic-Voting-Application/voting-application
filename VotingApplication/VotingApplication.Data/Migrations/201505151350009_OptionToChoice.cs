e616c3f8891deb717c106e3ceaf43c901608d1b2
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-05-19 14:16:10 +0100

    Fix migration to retain existing options when migrating to choices.

--- VotingApplication/VotingApplication.Data/Migrations/201505151350009_OptionToChoice.cs
+++ VotingApplication/VotingApplication.Data/Migrations/201505151350009_OptionToChoice.cs
@@ -1,8 +1,7 @@
 namespace VotingApplication.Data.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class OptionToChoice : DbMigration
     {
         public override void Up()
@@ -24,16 +23,21 @@ namespace VotingApplication.Data.Migrations
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Polls", t => t.Poll_Id)
                 .Index(t => t.Poll_Id);
-            
+
             AddColumn("dbo.Votes", "Choice_Id", c => c.Long());
             AddColumn("dbo.Polls", "ChoiceAdding", c => c.Boolean(nullable: false));
             CreateIndex("dbo.Votes", "Choice_Id");
             AddForeignKey("dbo.Votes", "Choice_Id", "dbo.Choices", "Id");
+
+            Sql(@"INSERT INTO dbo.Choices (Name, Description, PollChoiceNumber, Poll_Id)
+                 SELECT Name, Description, PollOptionNumber, Poll_Id
+                 FROM dbo.Options");
+
             DropColumn("dbo.Votes", "Option_Id");
             DropColumn("dbo.Polls", "OptionAdding");
             DropTable("dbo.Options");
         }
-        
+
         public override void Down()
         {
             CreateTable(
@@ -47,7 +51,7 @@ namespace VotingApplication.Data.Migrations
                         Poll_Id = c.Long(),
                     })
                 .PrimaryKey(t => t.Id);
-            
+
             AddColumn("dbo.Polls", "OptionAdding", c => c.Boolean(nullable: false));
             AddColumn("dbo.Votes", "Option_Id", c => c.Long());
             DropForeignKey("dbo.Choices", "Poll_Id", "dbo.Polls");
