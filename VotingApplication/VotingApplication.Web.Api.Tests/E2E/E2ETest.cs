c55a92488f63e00291515b03b300ba6a273f2218
John Dawes <jdawes@scottlogic.co.uk>
2015-05-26 14:19:24 +0100

    Add Unregistered Dashboard tests
    Add unregistered dashboard tests and base class with common functionality.

---
+++ VotingApplication/VotingApplication.Web.Api.Tests/E2E/E2ETest.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Protractor;
+using System;
+using System.Threading;
+
+namespace VotingApplication.Web.Api.Tests.E2E
+{
+    public class E2ETest
+    {
+        protected const string ChromeDriverDir = @"..\..\";
+        protected const string SiteBaseUri = @"http://localhost:64205/";
+
+        public NgWebDriver NgDriver
+        {
+            get
+            {
+                var driver = new NgWebDriver(new ChromeDriver(ChromeDriverDir));
+                driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(10));
+                driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(10));
+
+                return driver;
+            }
+        }
+
+        public IWebDriver Driver
+        {
+            get { return NgDriver; }
+        }
+
+        public static void GoToBaseUri(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(SiteBaseUri);
+        }
+
+        public static IWebElement FindElementById(IWebDriver driver, string elementId)
+        {
+            return driver.FindElement(By.Id(elementId));
+        }
+
+        public void WaitForPageChange()
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ VotingApplication/VotingApplication.Web.Api.Tests/E2E/UnregisteredDashboardTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace VotingApplication.Web.Api.Tests.E2E
+{
+    [TestClass]
+    public class UnregisteredDashboardTests : E2ETest
+    {
+        [TestMethod]
+        [TestCategory("E2E")]
+        public void QuickPoll_DisabledWhenQuestionHasNoText()
+        {
+            using (IWebDriver driver = Driver)
+            {
+                GoToBaseUri(driver);
+
+                IWebElement questionBox = FindElementById(driver, "question");
+                questionBox.Clear();
+
+                IWebElement quickPollButton = FindElementById(driver, "quickpoll-button");
+
+                Assert.IsFalse(quickPollButton.Enabled);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("E2E")]
+        public void QuickPoll_EnabledWhenQuestionHasText()
+        {
+            using (IWebDriver driver = Driver)
+            {
+                driver.Navigate().GoToUrl(SiteBaseUri);
+
+                IWebElement questionBox = FindElementById(driver, "question");
+                questionBox.SendKeys("?");
+
+                IWebElement quickPollButton = FindElementById(driver, "quickpoll-button");
+
+                Assert.IsTrue(quickPollButton.Enabled);
+            }
+        }
+    }
+}
