bf02a2589373f3d00fe1d2190d906bbf43375f8e
John Dawes <jdawes@scottlogic.co.uk>
2015-07-06 10:15:24 +0100

    Up Down vote
    Add up down vote.

---
+++ VotingApplication/VotingApplication.Web/Scripts/Directives/VoteChoice/VoteChoiceTypes/UpDownVoteChoice.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('VoteOn-Vote')
+        .directive('upDownVoteChoice', upDownVoteChoice);
+
+    function upDownVoteChoice() {
+        return {
+            restrict: 'E',
+            scope: {
+                choices: '='
+            },
+            templateUrl: '/Scripts/Directives/VoteChoice/VoteChoiceTypes/UpDownVoteChoice.html',
+            controller: UpDownChoiceController
+        };
+    }
+
+    UpDownChoiceController.$inject = ['$scope'];
+
+    function UpDownChoiceController($scope) {
+
+        $scope.downSelected = downSelected;
+        $scope.neitherSelected = neutralSelected;
+        $scope.upSelected = upSelected;
+
+        function downSelected(choice) {
+            choice.voteValue = -1;
+        }
+
+        function neutralSelected(choice) {
+            choice.voteValue = 0;
+        }
+
+        function upSelected(choice) {
+            choice.voteValue = 1;
+        }
+    }
+})();
