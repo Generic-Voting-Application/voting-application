27e4b5ce98a276b6726fe14c16844fc118cd2a83
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-07-07 10:08:43 +0100

    Move poll creation/validation logic to create controller

--- VotingApplication/VotingApplication.Web/Scripts/Controllers/Create/ConfirmStepController.js
+++ VotingApplication/VotingApplication.Web/Scripts/Controllers/Create/ConfirmStepController.js
@@ -7,30 +7,22 @@
         .module('VoteOn-Create')
         .controller('ConfirmStepController', ConfirmStepController);
 
-    ConfirmStepController.$inject = ['$scope', 'PollService', 'RoutingService'];
+    ConfirmStepController.$inject = ['$scope'];
 
-    function ConfirmStepController($scope, PollService, RoutingService) {
+    function ConfirmStepController($scope) {
 
-        $scope.getNonEmptyChoices = getNonEmptyChoices;
-        $scope.getValidInvitees = getValidInvitees;
         $scope.pollHasWarnings = pollHasWarnings;
         $scope.pollHasErrors = pollHasErrors;
         $scope.questionIsValid = questionIsValid;
         $scope.choicesAreValid = choicesAreValid;
         $scope.styleIsValid = styleIsValid;
         $scope.expiryIsValid = expiryIsValid;
-        $scope.invitationsAreValid = invitationsAreValid;
-        $scope.formatPollExpiry = formatPollExpiry;
-        $scope.expiryDateIsInPast = expiryDateIsInPast;
-        $scope.createPoll = createPoll;
-
-        var startingDate = moment().toDate();
-
+        
         function pollHasWarnings() {
             return !choicesAreValid() ||
                    !styleIsValid() ||
                    !expiryIsValid() ||
-                   !invitationsAreValid();
+                   !$scope.invitationsAreValid();
         }
 
         function pollHasErrors() {
@@ -47,7 +39,7 @@
         }
 
         function choicesAreValid() {
-            if (!$scope.newPoll.OptionAdding && getNonEmptyChoices().length === 0) {
+            if (!$scope.newPoll.OptionAdding && $scope.getNonEmptyChoices().length === 0) {
                 return false;
             }
 
@@ -63,109 +55,11 @@
                 return true;
             }
 
-            if (expiryDateIsInPast()) {
+            if ($scope.expiryDateIsInPast()) {
                 return false;
             }
 
             return true;
-        }
-
-        function invitationsAreValid() {
-            if ($scope.newPoll.InviteOnly && $scope.newPoll.Invitations.length === 0) {
-                return false;
-            }
-
-            return true;
-        }
-
-        function getNonEmptyChoices() {
-            if (!$scope.newPoll.Choices) {
-                return [];
-            }
-            return $scope.newPoll.Choices.filter(function (choice) {
-                return choice.Name;
-            });
-        }
-
-        function getValidInvitees() {
-            if (!$scope.newPoll.Invitations) {
-                return [];
-            }
-
-            return $scope.newPoll.Invitations.filter(function (invite) {
-                // Maybe this needs to be pulled out of this controller
-                var emailRegEx = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-                return emailRegEx.test(invite);
-            });
-        }
-
-        function formatPollExpiry(formatString) {
-
-            var Date = getPollExpiry();
-
-            if (Date) {
-                return moment(Date).format(formatString);
-            } else {
-                return 'Invalid Date';
-            }
-        }
-
-        function getPollExpiry() {
-            if (!$scope.newPoll.ExpiryDate) {
-                return null;
-            }
-
-            var date;
-
-            if (!$scope.newPoll.ExpiryDate) {
-                date = startingDate;
-            } else {
-                date = new Date($scope.newPoll.ExpiryDate);
-            }
-
-            return moment(date).toDate();
-        }
-
-        function getPollExpiryUtc() {
-            var date = getPollExpiry();
-
-            if (date) {
-                return moment(date).utc().toDate();
-            }
-
-            return null;
-        }
-
-        function expiryDateIsInPast() {
-            if (!$scope.newPoll.ExpiryDate) {
-                return false;
-            }
-
-            var date = new Date($scope.newPoll.ExpiryDate);
-            return (moment(date).isBefore(moment()));
-        }
-
-        function createPollModel() {
-            var poll = {};
-
-            poll.PollName = $scope.newPoll.PollName;
-            poll.Choices = getNonEmptyChoices();
-            poll.ChoiceAdding = $scope.newPoll.ChoiceAdding;
-            poll.PollType = $scope.newPoll.PollType;
-            poll.NamedVoting = $scope.newPoll.NamedVoting;
-            poll.ElectionMode = $scope.newPoll.ElectionMode;
-            poll.ExpiryDateUtc = getPollExpiryUtc();
-            poll.Invitations = getValidInvitees();
-            poll.InviteOnly = $scope.newPoll.InviteOnly;
-
-            return poll;
-        }
-
-        function createPoll() {
-            PollService.createPoll(createPollModel())
-                .then(function (response) {
-                    RoutingService.navigateToVotePage(response.UUID, response.CreatorBallot.TokenGuid);
-                });
         }
     }
 })();
--- VotingApplication/VotingApplication.Web/Scripts/Controllers/Create/CreateController.js
+++ VotingApplication/VotingApplication.Web/Scripts/Controllers/Create/CreateController.js
@@ -5,9 +5,9 @@
         .module('VoteOn-Create')
         .controller('CreateController', CreateController);
 
-    CreateController.$inject = ['$scope', '$timeout'];
+    CreateController.$inject = ['$scope', '$timeout', 'RoutingService', 'PollService'];
 
-    function CreateController($scope, $timeout) {
+    function CreateController($scope, $timeout, RoutingService, PollService) {
 
         $scope.newPoll = createDefaultPoll();
         $scope.currentStep = 0;
@@ -40,7 +40,13 @@
         ];
 
         $scope.advanceToStep = advanceToStep;
-
+        $scope.getNonEmptyChoices = getNonEmptyChoices;
+        $scope.getValidInvitees = getValidInvitees;
+        $scope.invitationsAreValid = invitationsAreValid;
+        $scope.formatPollExpiry = formatPollExpiry;
+        $scope.expiryDateIsInPast = expiryDateIsInPast;
+        $scope.createPoll = createPoll;
+        
         function advanceToStep(step) {
             var stepIndex = step - 1;
             for (var i = 0; i <= stepIndex; i++) {
@@ -63,6 +69,96 @@
                 OptionAdding: false,
                 ExpiryDate: null
             };
+        }
+
+        function invitationsAreValid() {
+            if ($scope.newPoll.InviteOnly && $scope.newPoll.Invitations.length === 0) {
+                return false;
+            }
+
+            return true;
+        }
+
+        function getNonEmptyChoices() {
+            if (!$scope.newPoll.Choices) {
+                return [];
+            }
+            return $scope.newPoll.Choices.filter(function (choice) {
+                return choice.Name;
+            });
+        }
+
+        function getValidInvitees() {
+            if (!$scope.newPoll.Invitations) {
+                return [];
+            }
+
+            return $scope.newPoll.Invitations.filter(function (invite) {
+                // Maybe this needs to be pulled out of this controller
+                var emailRegEx = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+                return emailRegEx.test(invite);
+            });
+        }
+
+        function formatPollExpiry(formatString) {
+
+            var Date = getPollExpiry();
+
+            if (Date) {
+                return moment(Date).format(formatString);
+            } else {
+                return 'Invalid Date';
+            }
+        }
+
+        function getPollExpiry() {
+            if (!$scope.newPoll.ExpiryDate) {
+                return null;
+            }
+
+            return moment($scope.newPoll.ExpiryDate).toDate();
+        }
+
+        function getPollExpiryUtc() {
+            var date = getPollExpiry();
+
+            if (date) {
+                return moment(date).utc().toDate();
+            }
+
+            return null;
+        }
+
+        function expiryDateIsInPast() {
+            if (!$scope.newPoll.ExpiryDate) {
+                return false;
+            }
+
+            var date = new Date($scope.newPoll.ExpiryDate);
+            return (moment(date).isBefore(moment()));
+        }
+
+        function createPollModel() {
+            var poll = {};
+
+            poll.PollName = $scope.newPoll.PollName;
+            poll.Choices = getNonEmptyChoices();
+            poll.ChoiceAdding = $scope.newPoll.ChoiceAdding;
+            poll.PollType = $scope.newPoll.PollType;
+            poll.NamedVoting = $scope.newPoll.NamedVoting;
+            poll.ElectionMode = $scope.newPoll.ElectionMode;
+            poll.ExpiryDateUtc = getPollExpiryUtc();
+            poll.Invitations = getValidInvitees();
+            poll.InviteOnly = $scope.newPoll.InviteOnly;
+
+            return poll;
+        }
+
+        function createPoll() {
+            PollService.createPoll(createPollModel())
+                .then(function (response) {
+                    RoutingService.navigateToVotePage(response.UUID, response.CreatorBallot.TokenGuid);
+                });
         }
     }
 })();
