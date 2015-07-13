ebf5b3e96005ba15aa5d6cd820fe7f0cb2e122b0
John Dawes <jdawes@scottlogic.co.uk>
2015-07-13 12:47:51 +0100

    Change homepage over to new one

--- VotingApplication/VotingApplication.Web/App_Start/RouteConfig.cs
+++ VotingApplication/VotingApplication.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace VotingApplication.Web
             routes.MapRoute(
                 name: "DefaultAction",
                 url: "{controller}/{action}/{id}/{token}",
-                defaults: new { controller = "Manage", action = "Index", id = UrlParameter.Optional, token = UrlParameter.Optional }
+                defaults: new { controller = "Create", action = "Index", id = UrlParameter.Optional, token = UrlParameter.Optional }
             );
         }
     }
