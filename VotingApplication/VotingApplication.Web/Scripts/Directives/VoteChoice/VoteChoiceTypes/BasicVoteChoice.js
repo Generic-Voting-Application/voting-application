87b20a7a1d06e980ca29363fb768f14b9bfc50b3
John Dawes <jdawes@scottlogic.co.uk>
2015-07-16 11:58:55 +0100

    Update to basic poll vote
    Selected vote is now md-accent.

--- VotingApplication/VotingApplication.Web/Scripts/Directives/VoteChoice/VoteChoiceTypes/BasicVoteChoice.js
+++ VotingApplication/VotingApplication.Web/Scripts/Directives/VoteChoice/VoteChoiceTypes/BasicVoteChoice.js
@@ -19,18 +19,42 @@
     BasicChoiceController.$inject = ['$scope'];
 
     function BasicChoiceController($scope) {
+
+        $scope.anyChoiceSelected = false;
+
+        $scope.shouldShowSelectedCheck = shouldShowSelectedCheck;
+        $scope.shouldOffsetchoice = shouldOffsetchoice;
         $scope.choiceSelected = choiceSelected;
 
+        activate();
+
+        function activate() {
+            $scope.anyChoiceSelected = $scope.choices.some(function (elem) {
+                return elem.VoteValue === 1;
+            });
+        }
+
         function choiceSelected(choice) {
+
+            $scope.anyChoiceSelected = false;
 
             $scope.choices.map(function (element) {
                 if (element === choice && choice.VoteValue === 0) {
                     choice.VoteValue = 1;
+                    $scope.anyChoiceSelected = true;
                 } else {
                     element.VoteValue = 0;
                 }
             });
+        }
 
+        function shouldShowSelectedCheck(choice) {
+            return !$scope.anyChoiceSelected || choice.VoteValue === 1;
+        }
+
+
+        function shouldOffsetchoice(choice) {
+            return $scope.anyChoiceSelected && choice.VoteValue === 0;
         }
     }
 })();
