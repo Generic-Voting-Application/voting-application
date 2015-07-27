3c30f71d7a739a9ac1da61f3cfa9331e038e9fbd
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-07-27 11:54:11 +0100

    Create unique index over ballot and choice for votes to prevent duplicate votes.

--- VotingApplication/VotingApplication.Data/Model/Vote.cs
+++ VotingApplication/VotingApplication.Data/Model/Vote.cs
@@ -1,10 +1,13 @@
-﻿namespace VotingApplication.Data.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace VotingApplication.Data.Model
 {
     public class Vote
     {
         public long Id { get; set; }
+        [Index("IX_ChoiceBallot", 0, IsUnique = true)]
         public Choice Choice { get; set; }
         public int VoteValue { get; set; }
+        [Index("IX_ChoiceBallot", 1, IsUnique = true)]
         public Ballot Ballot { get; set; }
 
         public Poll Poll { get; set; }
